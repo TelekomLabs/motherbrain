75bfe8226b0464402885189ae1e1edb370699552
Jamie Winsor <jamie@vialstudios.com>
2013-05-24 14:05:14 -0700

    pick a quote style and stick to it in the plugin template

--- templates/motherbrain.rb
+++ templates/motherbrain.rb
@@ -10,13 +10,13 @@
 # components and groups you want to bootstrap.
 stack_order do
 <% config[:groups].each do |group| -%>
-  bootstrap 'app::<%= group %>'
+  bootstrap "app::<%= group %>"
 <% end -%>
 end
 
 # Components are logical parts of your application. For instance, a web app
 # might have "web" and "database" components.
-component 'app' do
+component "app" do
   # Replace this with a better description for the component.
   description "<%= config[:name].capitalize %> application"
 
@@ -25,20 +25,20 @@ component 'app' do
   #
   #   mb <%= config[:name] %> upgrade --components app:1.2.3
   #
-  versioned # This defaults to 'app.version'
+  versioned # This defaults to "app.version"
   # You can also specify a custom attribute.
-  # versioned_with '<%= config[:name] %>.version'
+  # versioned_with "<%= config[:name] %>.version"
 
   # Groups are collections of nodes linked by a search. If you only have one
   # group per component, it's typical to use "default" as the group name.  An
   # example of multiple groups would be a "database" component, with "master"
   # and "slave" groups.
-  group 'default' do
+  group "default" do
 <% config[:groups].each do |group| -%>
-    recipe '<%= config[:name] %>::<%= group %>'
+    recipe "<%= config[:name] %>::<%= group %>"
 <% end -%>
     # In addition to recipes, you can also search by roles and attributes:
-    # role 'web_server'
-    # chef_attribute 'db_master', true
+    # role "web_server"
+    # chef_attribute "db_master", true
   end
 end
